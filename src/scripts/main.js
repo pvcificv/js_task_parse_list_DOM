54ce10ebd5548838073ffdc8313d761e47cf8e03
pvcificv <pvcificv@quantum.gate>
2025-09-22 16:25:48 +0200

    first task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
-'use strict';
+function sortList() {
+  const list = document.querySelector('ul');
+  const items = Array.from(list.querySelectorAll('li'));
 
-// write code here
+  items.sort((a, b) => {
+    const salaryA = Number(a.dataset.salary.replace(/\D/g, ''));
+    const salaryB = Number(b.dataset.salary.replace(/\D/g, ''));
+
+    return salaryB - salaryA;
+  });
+
+  items.forEach((item) => list.appendChild(item));
+}
+
+function getEmployeesArray() {
+  const items = document.querySelectorAll('ul li');
+
+  return Array.from(items).map((li) => ({
+    name: li.textContent.trim(),
+    position: li.dataset.position,
+    salary: Number(li.dataset.salary.replace(/\D/g, '')),
+    age: Number(li.dataset.age),
+  }));
+}
+
+sortList();
+
+const employees = getEmployeesArray();
+
+print(employees);
